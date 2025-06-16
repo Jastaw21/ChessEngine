ce033fe2cbfae5d6070614a50f50d53b4af91326
Jack Weddell <jacksweddell@outlook.com>
2025-06-16 18:43:15 +0100

    Add Vec2D class

---
+++ include/Vec2D.h
@@ -0,0 +1,36 @@
+//
+// Created by jacks on 16/06/2025.
+//
+
+#ifndef VEC2D_H
+#define VEC2D_H
+
+
+
+class Vec2D {
+    public:
+    float x,y;
+
+    Vec2D operator+(const Vec2D &v) const {
+        return Vec2D(x + v.x, y + v.y);
+    }
+    Vec2D operator-(const Vec2D &v) const {
+        return Vec2D(x - v.x, y - v.y);
+    }
+
+    Vec2D operator/(const float &v) const {
+        return Vec2D(x / v, y / v);
+    }
+
+
+    Vec2D operator+=(const Vec2D &rhs) {x += rhs.x; y += rhs.y;
+        return *this;
+    }
+    Vec2D operator-=(const Vec2D &rhs) {x -= rhs.x; y -= rhs.y;
+        return *this;
+    }
+};
+
+
+
+#endif //VEC2D_H
