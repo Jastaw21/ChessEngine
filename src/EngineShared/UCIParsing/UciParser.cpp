916a0f88169ca5c7561586e716084bc47f3c90c5
Jastaw21 <96827140+Jastaw21@users.noreply.github.com>
2025-07-16 15:04:22 +0100

    Update UciParser.cpp

--- src/EngineShared/UCIParsing/UciParser.cpp
+++ src/EngineShared/UCIParsing/UciParser.cpp
@@ -5,6 +5,28 @@
 
 #include "EngineShared/UCIParsing/Tokeniser.h"
 
+void UciParser::parse(const std::string& inString){
+    currentToken = 0;
+    auto tokens = Tokeniser(inString);
+
+    while (currentToken < tokens.size()){
 
 
+        currentToken++; // advance
+    }
+    
+    return;
+};
 
+Token& UciParser::peek(){
+    if (currentToken+1 < tokens.size()){
+        return tokens.at(currentToken);
+    }
+    else{
+        return Token{.type = EOF_TOKEN,.value = ""};
+    }
+}
+
+void UciParser::parseMoveList(){
+    
+}
