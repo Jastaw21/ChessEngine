05f4ed1dbb0852a553e8025c005f1273051234b1
jack <jack.weddell@ao.com>
2025-11-06 14:17:55 +0000

    Add opening book

---
+++ include/GUI/OpeningBook.h
@@ -0,0 +1,36 @@
+#ifndef OPENINGBOOK_H
+#define OPENINGBOOK_H
+#include <random>
+#include <filesystem>
+
+class OpeningBook
+{
+
+public:
+    OpeningBook() 
+    : rng(std::chrono::system_clock::now().time_since_epoch().count()) 
+    {
+        obFile = std::ifstream(OPENING_BOOK_FILE);
+        std::string line;
+        while (std::getline(obFile, line)){
+        openingPositions.push_back(line);
+        }
+    }
+
+
+    FenString GetRandomFen(){
+        int line = rng() % openingPositions.size();
+        return openingPositions[line];
+    }
+
+private:
+    std::mt19937 rng;
+    std::ifstream obFile;
+ 
+
+    std::vector<FenString> openingPositions;
+};
+
+
+
+#endif
